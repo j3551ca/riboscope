cad919d0ac1bdbd2e2cf9bdde873b911b93f28e4
j3551ca <citrusty543@gmail.com>
2025-09-26 11:53:29 -0700

    Add functions to generate inputs for final report

--- bin/report_results.py
+++ bin/report_results.py
@@ -6,74 +6,118 @@ import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import seaborn as sns
 
-def ingest_qc_results(amp_df, annotated_vcf):
-
-    reportable_vcf = pd.read_csv(annotated_vcf, header=0)
+def ingest_qc_results(qc_summary_file, amp_df, annotated_vcf, failed_amplicons):
+    """
+    Read in QC results as pandas.DataFrames for use in html report generation.
+    """
+    qc_df = pd.read_csv(qc_summary_file, header=0)
+    reportable_vcf = pd.read_csv(annotated_vcf, header=0, sep="\t")
     amplicon_df = pd.read_csv(amp_df, header=0)
+    failed_amplicon_df = pd.read_csv(failed_amplicons, header=0)
 
-    return(amplicon_df, reportable_vcf)
+    #prep vcf table for viz & summarizing 
+    reportable_vcf["snv"] = reportable_vcf["ref"].astype(str) + reportable_vcf["pos"].astype(str) + reportable_vcf["alt"]
+    reportable_vcf = reportable_vcf.sort_values("pos", ascending=True)
+    snv_counts = reportable_vcf.groupby("snv")["sample_id"].nunique().rename("count")
+    reportable_vcf = reportable_vcf.merge(snv_counts, on="snv", how="left")
+    #total samples with successfully sequenced pool that contain each SNV
+    total_samples = reportable_vcf.groupby(["pool"])["sample_id"].nunique().rename("total_pool_count")
+    reportable_vcf = reportable_vcf.merge(total_samples, on=["pool"], how = "left")
+    reportable_vcf["sample_count"] = reportable_vcf["count"].astype(str) + "/" + reportable_vcf["total_pool_count"].astype(str)
+    reportable_vcf["vaf"] = (reportable_vcf["af"]*100).round(1).astype(str) + "%"
 
-def plot_vcf(repeat_annotated_vcf):
+    return(qc_df, amplicon_df, reportable_vcf, failed_amplicon_df)
 
+
+def summarize_results(qc_summary, vcf_df):
+    """
+    Summarize sample pass/ fail and SNP results - results overview.
+    """
+
+    vcf_df["pool"]=vcf_df["pool"].replace({"pool1":"16S","pool2":"23S"})
+
+    results_df = pd.DataFrame({"metric":["Total Number of Samples", 
+                            "Number of Failed Samples", 
+                            "SNPs ≥90% Frequency", 
+                            "SNPs ≥50% Samples"],
+                 "results": [len(qc_summary["sample_id"]), 
+                             len(qc_summary[qc_summary["qc_fail"]]), 
+                             ",<br>".join(vcf_df[vcf_df["af"]>=0.9][["snv", "pool"]]
+                                        .drop_duplicates().astype(str).agg("_".join, axis=1)),
+                             ",<br>".join(vcf_df[(vcf_df["count"]/vcf_df["total_pool_count"])>=0.5][["snv", "pool"]]
+                                        .drop_duplicates().astype(str).agg("_".join, axis=1))]})
+    
+    fail_flags = qc_summary[qc_summary["flags"]!="[]"][["sample_id", "flags"]]
+    
+    results_df=results_df.to_html(index=False, escape=False, classes="pretty-table")
+    fail_flags=fail_flags.to_html(index=False, escape=False, classes="pretty-table")
+
+    return results_df, fail_flags
+
+
+def plot_amplicons(failed_amps):
+    """
+    Heatmap of (boolean) amplicon success for each sample.
+    """
+    failed_amps_wide = failed_amps.pivot(index="sample_id", columns="amplicon_name", values="amplicon_fail")
+
+    failed_order = failed_amps_wide.eq(1).sum(axis=1).sort_values(ascending=False)
+    failed_amps_wide = failed_amps_wide.loc[failed_order.index]
+    display = failed_amps_wide.replace({0: "Pass", 1: "Fail"})
+
+    fig = px.imshow(
+        failed_amps_wide,
+        aspect="auto",
+        text_auto=".0f",
+        color_continuous_scale=["#1cba56", "#c93808"],
+        labels=dict(x="Sequence", y="Sample")
+    )
+
+    fig.update_traces(showscale=False,
+                        text=display.to_numpy(),
+                        texttemplate="%{text}",
+                        hoverongaps=False,
+                        xgap=1, ygap=1)
+
+    fig.update_layout(title="Sequencing Success of Amplicons",
+                        coloraxis_showscale=False,
+                        autosize=True,
+                        height=300)
+    return fig.to_html(full_html=False, include_plotlyjs="cdn")
+
+
+def plot_vcf(vcf_df):
     """
     Plot SNPs by pool and represent repeats present in pool with shape
     """
-    repeat_annotated_vcf
-    max_count_pool1 = df_pool1["COUNT"].max()
-    max_count_pool2 = df_pool2["COUNT"].max()
-    filter_pool1 = df_pool1[(df_pool1["COUNT"]>=5)| (df_pool1["AF"]>=0.01)]
-    filter_pool2 = df_pool2[(df_pool2["COUNT"]>=5) | (df_pool2["AF"]>=0.01)]
+    fig = px.scatter(vcf_df, 
+            x = "snv", 
+            y = "af",
+            color="repeat_status",  # optional categorical coloring
+            opacity=0.6,
+            size="count",
+            size_max=8,
+            hover_data={"snv":True, "af":False,"vaf":True, "sample_id":True, "count":False,"sample_count":True, "pool":False},  # info shown on hover
+            facet_col="pool",
+            log_y=True,
+            title="rRNA SNVs Across Samples",
+            template="plotly_white")
 
-    filter_pool1 = filter_pool1.sort_values("POS", ascending=True)
-    filter_pool2 = filter_pool2.sort_values("POS", ascending=True)
+    fig.update_xaxes(matches=None)
+    fig.update_xaxes(title_text="", row=1, col=1)
+    fig.update_xaxes(title_text="", row=1, col=2)
+    fig.update_layout(
+        xaxis_title="",
+        title_x=0.5,
+        height=600,
+        yaxis_title="Allele Frequency",
+        legend_title_text="Ribosomal Repeat")
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
 
-    plt.clf()
-    fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2,
-                                figsize=(12, 8), sharey=True)
-
-    ax1.grid(axis='x', linestyle="--", alpha=0.3)
-    ax1.grid(axis='y', linestyle="--", alpha=0.3)
-    sc1 = ax1.scatter(filter_pool1["SNV"], filter_pool1["AF"],
-                s=filter_pool1["COUNT"],
-                alpha=0.5)
-    ax1.set_xticklabels(ax1.get_xticklabels(), va = "top",
-                        ha="right")
-    ax1.tick_params(axis="x", labelsize=6, 
-                    labelrotation=45)
-    ax1.set_xlabel("Pool 1")
-    ax1.set_yscale("log")
-
-    # now automatically pick 4 sizes from min→max and label them
-    handles1, labels1 = sc1.legend_elements(prop="sizes", num=5, fmt="{x:.0f}", color="#1f77b4", alpha =0.7)
-    ax1.legend(handles1, labels1, title="Samples", loc="upper left")
+    return fig.to_html(full_html=False, include_plotlyjs="cdn")
 
 
-    ax2.grid(axis='x', linestyle="--", alpha=0.3)
-    ax2.grid(axis='y', linestyle="--", alpha=0.3)
-    sc2 = ax2.scatter(filter_pool2["SNV"], filter_pool2["AF"],
-                s=filter_pool2["COUNT"],
-                alpha=0.5)
-    ax2.set_yticks(np.arange(0, 1.0, 0.1))
-    ax2.set_xticklabels(ax2.get_xticklabels(),va="top",
-                        ha="right")
-    ax2.tick_params(axis="x", labelsize=6, 
-                    labelrotation=45)
-    ax2.set_xlabel("\n\n\n\n\n\nPool 2")
-    ax2.set_yscale("log")
-
-    handles2, labels2 = sc2.legend_elements(prop="sizes", num=5, fmt="{x:.0f}", color="#1f77b4", alpha=0.7)
-    ax2.legend(handles2, labels2, title="Samples", loc="upper left")
-
-    fig.text(0.05, 0.5, "Allele Frequency", ha="center", fontsize=12, rotation=90)
-    fig.text(0.5, 0.04, "SNV", ha="center", fontsize=12)
-    fig.text(0.4, 0.95, "SNVs ≥1% Frequency or ≥5 Samples Called")
-    plt.tight_layout(rect=[0.05, 0.05, 1, 0.95])
-    plt.show()
-
-    return plt
-
-def plot_amplicons(raw_counts):
-
+def plot_heatmap(raw_counts):
     """
     Heatmap of query sequence hits - proxy for presence/ absence of amplicons sequenced.
     """
